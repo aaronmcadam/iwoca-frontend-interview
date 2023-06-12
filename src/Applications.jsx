6aa7f5be91c11d48afe3982c6afd91975a8f3156
Aaron McAdam <aaron@aaronmcadam.com>
2023-06-12 16:28:26 +0100

    initial setup
    kicking the tires with CSS modules.

--- src/Applications.jsx
+++ src/Applications.jsx
@@ -2,12 +2,17 @@ import React from "react";
 import SingleApplication from "./SingleApplication";
 import { getSingleApplicationFixture } from "./__fixtures__/applications.fixture";
 import styles from "./Applications.module.css";
+import { Button } from "./ui/Button/Button";
 
 const Applications = () => {
   const applications = getSingleApplicationFixture;
+
   return (
     <div className={styles.Applications}>
       <SingleApplication application={applications[0]} />
+      <div className={styles.Actions}>
+        <Button>Load more</Button>
+      </div>
     </div>
   );
 };
